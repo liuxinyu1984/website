7b936301814ff9b3cb1cf0a874b51930a219d41c
liuxinyu1984 <liuxinyu1984@users.noreply.github.com>
2023-04-04 23:55:44 -0700

    add post_date field, order homepage by reversed date

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -10,7 +10,8 @@ from django.urls import reverse_lazy
 class HomeView(ListView):
     model = Post
     template_name = 'home.html'
-    ordering = ['-id']
+    #ordering = ['-id']
+    ordering = ['-post_date']
 
 class ArticleDetailView(DetailView):
     model = Post
