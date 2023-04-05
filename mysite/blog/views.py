4d88295e4aed5b2facf98d4e8abe583aecbbf472
liuxinyu1984 <liuxinyu1984@users.noreply.github.com>
2023-04-04 23:08:22 -0700

    add delete page

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
 from .forms import AddPostForm, EditPostForm
+from django.urls import reverse_lazy
 
 # def home(request):
 #     return render(request, 'home.html', {})
@@ -9,6 +10,7 @@ from .forms import AddPostForm, EditPostForm
 class HomeView(ListView):
     model = Post
     template_name = 'home.html'
+    ordering = ['-id']
 
 class ArticleDetailView(DetailView):
     model = Post
@@ -26,3 +28,9 @@ class UpdatePostView(UpdateView):
     form_class = EditPostForm
     template_name = 'update_post.html'
     #fields = ['title', 'body']
+
+
+class DeletePostView(DeleteView):
+    model = Post
+    template_name = 'delete_post.html'
+    success_url = reverse_lazy('home')
